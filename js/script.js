177163baaa61f0ce613db61dbb579c697884e060
RafalSzymanskiKBH <rafiskce@gmail.com>
2023-03-11 22:12:01 +0100

    update javaScript

--- js/script.js
+++ js/script.js
@@ -1,32 +1,43 @@
-console.log("Czesc!");
+   console.log("Czesc!");
+{
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const changeDark = document.querySelector(".js-changeDark");
+        body.classList.toggle("cs--dark");
 
-const section__buttonPicture = document.querySelector(".section__buttonPicture");
-const table__button = document.querySelector(".table__button");
-const navigation__dark = document.querySelector(".navigation__dark");
-const body = document.querySelector(".body");
-const table__gym = document.querySelector(".table__gym");
-const header__pictureSport = document.querySelector(".header__pictureSport");
-const changeDark = document.querySelector(".changeDark");
-const changeTable = document.querySelector(".changeTable");
-const changePicture = document.querySelector(".changePicture");
+        changeDark.innerText =
+            (body.classList.contains("cs--dark")) ? "jasny" : "ciemny";
+    };
 
-table__button.addEventListener("click", () => {
-    table__gym.classList.toggle("js--changeTable");
+    const toggleTable = () => {
+        const table__gym = document.querySelector(".js--table__gym");
+        const changeTable = document.querySelector(".js--changeTable");
 
-    changeTable.innerText =
-        (table__gym.classList.contains("js--changeTable")) ? "Otworz" : "Usun";
+        table__gym.classList.toggle("js--changeTable");
 
-});
+        changeTable.innerText =
+            (table__gym.classList.contains("js--changeTable")) ? "Otworz" : "Usun";
+    };
 
-section__buttonPicture.addEventListener("click", () => {
-    header__pictureSport.classList.toggle("js--changePicture");
+    const togglePicture = () => {
+        const header__pictureSport = document.querySelector(".js--header__pictureSport");
+        const changePicture = document.querySelector(".js--changePicture");
+        header__pictureSport.classList.toggle("js--changePicture");
 
-    changePicture.innerText =
-        (header__pictureSport.classList.contains("js--changePicture")) ? "Otworz" : "Usun";
-});
-navigation__dark.addEventListener("click", () => {
-    body.classList.toggle("cs--dark");
+        changePicture.innerText =
+            (header__pictureSport.classList.contains("js--changePicture")) ? "Otworz" : "Usun";
+        
+    };
 
-    changeDark.innerText =
-        (body.classList.contains("cs--dark")) ? "jasny" : "ciemny";
-});
+    const init = () => {
+        const navigation__dark = document.querySelector(".js--navigation__buttonDark");
+        navigation__dark.addEventListener("click", toggleBackground);
+        const table__button = document.querySelector(".js--table__button");
+        table__button.addEventListener("click", toggleTable);
+        const section__buttonPicture = document.querySelector(".js--section__buttonPicture");
+        section__buttonPicture.addEventListener("click", togglePicture);
+    };
+
+    init();
+
+}
